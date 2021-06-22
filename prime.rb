805ab8c59c6162ce60f9cf262e069130967a51e2
sbalayan1 <balayans2014@yahoo.com>
2021-06-22 14:13:30 -0700

    commit tree b8770cc262c55c479fcc084f286594f6490a2391

--- prime.rb
+++ prime.rb
@@ -1,1 +1,24 @@
 # Add  code here!
+require 'pry'
+
+def prime? number
+    if number > 1
+        new_array = []
+        (2..(number-1)).map do |num| 
+            if number % num == 0 
+                new_array << false 
+            else 
+                new_array << true
+            end
+        end
+
+        if new_array.include?(false)
+            false
+        else 
+            true
+        end 
+    else 
+        false
+    end
+end 
+
